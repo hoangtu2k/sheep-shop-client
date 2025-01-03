290ceecaa5ce4182fa754ae99bfdb38cb75c8218
hoangtu2k <thocodeonline@gmail.com>
2025-01-03 21:59:07 +0700

    update

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -17,20 +17,20 @@ const Sidebar = () => {
 
     const context = useContext(MyContext);
 
-    const isOpenSubmenu=(index)=> {
+    const isOpenSubmenu = (index) => {
         setActiveTab(index);
         setIsToggleSubmenu(!isToggleSubmenu);
     }
 
-  const navigate = useNavigate(); // Khai báo hook navigate
+    const navigate = useNavigate(); // Khai báo hook navigate
 
-  const handleLogout = () => {
-    // Xoá token khỏi Local Storage
-    localStorage.removeItem('token');
-  
-    // Điều hướng đến trang đăng nhập hoặc trang chính
-    navigate('/admin/login'); // Sử dụng navigate để điều hướng
-  };
+    const handleLogout = () => {
+        // Xoá token khỏi Local Storage
+        localStorage.removeItem('token');
+
+        // Điều hướng đến trang đăng nhập hoặc trang chính
+        navigate('/admin/login'); // Sử dụng navigate để điều hướng
+    };
 
 
     return (
@@ -39,7 +39,7 @@ const Sidebar = () => {
                 <ul>
                     <li>
                         <Link to="/admin/dashboard">
-                            <Button className={`w-100 ${activeTab === 0 ? 'active': ''}`}>
+                            <Button className={`w-100 ${activeTab === 0 ? 'active' : ''}`}>
                                 <span className="icon"><MdDashboard /></span>
                                 Tổng quan
                                 <span className="arrow"></span>
@@ -47,63 +47,66 @@ const Sidebar = () => {
                         </Link>
                     </li>
                     <li>
-                        
-                            <Button className={`w-100 ${activeTab === 1 && isToggleSubmenu ===true ? 'active': ''}`} onClick={()=>isOpenSubmenu(1)}>
-                                <span className="icon"><FaProductHunt /></span>
-                                Sản phẩm
-                                <span className="arrow"><FaAngleRight /></span>
-                            </Button>
-                            
-                            <div className={`submenuWrapper ${activeTab === 1 && isToggleSubmenu ===true ? 'colapse': 'colapsed'} `}>
-                                <ul className="submenu">
-                                    <li><Link to="/admin/products">Danh sách sản phẩm</Link></li>
-                                    <li><Link to="/admin/product/details">Chi tiết sản phẩm</Link></li>
-                                    <li><Link to="/admin/product/upload">Tải lên sản phẩm</Link></li>
-                                </ul>
-                            </div>
-                                  
+                        <Button className={`w-100 ${activeTab === 1 && isToggleSubmenu === true ? 'active' : ''}`} onClick={() => isOpenSubmenu(1)}>
+                            <span className="icon"><FaProductHunt /></span>
+                            Sản phẩm
+                            <span className="arrow"><FaAngleRight /></span>
+                        </Button>
+
+                        <div className={`submenuWrapper ${activeTab === 1 && isToggleSubmenu === true ? 'colapse' : 'colapsed'} `}>
+                            <ul className="submenu">
+                                <li><Link to="/admin/products">Danh sách sản phẩm</Link></li>
+                                <li><Link to="/admin/product/details">Chi tiết sản phẩm</Link></li>
+                                <li><Link to="/admin/product/upload">Tải lên sản phẩm</Link></li>
+                            </ul>
+                        </div>
+
                     </li>
                     <li>
-                        <Link to="/">
-                            <Button className={`w-100 ${activeTab === 2 ? 'active': ''}`}>
-                                <span className="icon"><IoMdSwap /></span>
-                                Giao dịch
-                                <span className="arrow"><FaAngleRight /></span>
-                            </Button>
-                        </Link>
+                        <Button className={`w-100 ${activeTab === 2 && isToggleSubmenu === true ? 'active' : ''}`} onClick={() => isOpenSubmenu(2)}>
+                            <span className="icon"><IoMdSwap /></span>
+                            Giao dịch
+                            <span className="arrow"><FaAngleRight /></span>
+                        </Button>
+                        <div className={`submenuWrapper ${activeTab === 2 && isToggleSubmenu === true ? 'colapse' : 'colapsed'} `}>
+                            <ul className="submenu">
+                                <li><Link to="/orders">Đặt hàng</Link></li>
+                                <li><Link to="/invoices">Hóa đơn</Link></li>
+                            </ul>
+                        </div>
                     </li>
-                    <li>                      
-                            <Button className={`w-100 ${activeTab === 3 && isToggleSubmenu ===true ? 'active': ''}`} onClick={()=>isOpenSubmenu(3)}>
-                                <span className="icon"><HiUsers /></span>
-                                Nhân viên
-                                <span className="arrow"><FaAngleRight /></span>
-                            </Button>
-                            
-                            <div className={`submenuWrapper ${activeTab === 3 && isToggleSubmenu ===true ? 'colapse': 'colapsed'} `}>
-                                <ul className="submenu">
-                                    <li><Link to="/admin/users">Danh sách nhân viên</Link></li>
-                                    <li><Link to="/">Lịch làm việc</Link></li>
-                                    <li><Link to="/">Thiết lập nhân viên</Link></li>
-                                </ul>
-                            </div>
+                    <li>
+                        <Button className={`w-100 ${activeTab === 3 && isToggleSubmenu === true ? 'active' : ''}`} onClick={() => isOpenSubmenu(3)}>
+                            <span className="icon"><HiUsers /></span>
+                            Nhân viên
+                            <span className="arrow"><FaAngleRight /></span>
+                        </Button>
+
+                        <div className={`submenuWrapper ${activeTab === 3 && isToggleSubmenu === true ? 'colapse' : 'colapsed'} `}>
+                            <ul className="submenu">
+                                <li><Link to="/admin/users">Danh sách nhân viên</Link></li>
+                                <li><Link to="/">Lịch làm việc</Link></li>
+                                <li><Link to="/">Thiết lập nhân viên</Link></li>
+                            </ul>
+                        </div>
                     </li>
-                    <li>                      
-                            <Button className={`w-100 ${activeTab === 4 && isToggleSubmenu ===true ? 'active': ''}`} onClick={()=>isOpenSubmenu(4)}>
-                                <span className="icon"><FaUserTie /></span>
-                                Đối tác
-                                <span className="arrow"><FaAngleRight /></span>
-                            </Button>
-                            
-                            <div className={`submenuWrapper ${activeTab === 4 && isToggleSubmenu ===true ? 'colapse': 'colapsed'} `}>
-                                <ul className="submenu">
-                                    <li><Link to="/admin/customers">Khách hàng</Link></li>
-                                    <li><Link to="/admin">Nhà cung cáp</Link></li>
-                                </ul>
-                            </div>
+                    <li>
+                        <Button className={`w-100 ${activeTab === 4 && isToggleSubmenu === true ? 'active' : ''}`} onClick={() => isOpenSubmenu(4)}>
+                            <span className="icon"><FaUserTie /></span>
+                            Đối tác
+                            <span className="arrow"><FaAngleRight /></span>
+                        </Button>
+
+                        <div className={`submenuWrapper ${activeTab === 4 && isToggleSubmenu === true ? 'colapse' : 'colapsed'} `}>
+                            <ul className="submenu">
+                                <li><Link to="/admin/customers">Khách hàng</Link></li>
+                                <li><Link to="/admin">Nhà cung cáp</Link></li>
+                            </ul>
+                        </div>
                     </li>
                     <li>
                         <Link to="/sale">
-                            <Button className={`w-100 ${activeTab === 5 ? 'active': ''}`}>
+                            <Button className={`w-100 ${activeTab === 5 ? 'active' : ''}`}>
                                 <span className="icon"><FaShoppingCart /></span>
                                 Bán hàng
                             </Button>
@@ -111,7 +114,7 @@ const Sidebar = () => {
                     </li>
                     <li>
                         <Link to="/">
-                            <Button className={`w-100 ${activeTab === 9 ? 'active': ''}`}>
+                            <Button className={`w-100 ${activeTab === 9 ? 'active' : ''}`}>
                                 <span className="icon"><MdMessage /></span>
                                 Tin nhắn
                                 <span className="arrow"><FaAngleRight /></span>
@@ -120,30 +123,28 @@ const Sidebar = () => {
                     </li>
                     <li>
                         <Link to="/">
-                            <Button className={`w-100 ${activeTab === 8 ? 'active': ''}`}>
+                            <Button className={`w-100 ${activeTab === 8 ? 'active' : ''}`}>
                                 <span className="icon"><FaBell /></span>
                                 Thông báo
-                                <span className="arrow"><FaAngleRight /></span>
                             </Button>
                         </Link>
                     </li>
                     <li>
                         <Link to="/">
-                            <Button className={`w-100 ${activeTab === 7 ? 'active': ''}`}>
+                            <Button className={`w-100 ${activeTab === 7 ? 'active' : ''}`}>
                                 <span className="icon"><IoIosSettings /></span>
                                 Cài đặt
-                                <span className="arrow"><FaAngleRight /></span>
                             </Button>
                         </Link>
                     </li>
 
                 </ul>
 
-                <br/>
+                <br />
 
                 <div className="logoutWrapper">
                     <div className="logoutBox">
-                        <Button variant="contained" onClick={handleLogout}><IoMdLogOut/> Đăng xuất</Button>
+                        <Button variant="contained" onClick={handleLogout}><IoMdLogOut /> Đăng xuất</Button>
                     </div>
                 </div>
 
